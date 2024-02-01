184989012729ef8beac15de0ca97251c1cc18c5f
kadoum amine <kadoumamine@gmail.com>
2024-02-02 00:30:19 +0100

    adjust workschema resume

--- client/src/components/works.jsx
+++ client/src/components/works.jsx
@@ -21,21 +21,26 @@ export const Works = [
     type: "web",
     image: opensource,
     title: "Open Source website",
+    resume:
+      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
     weblink: "https://open-source-day-2023.cse.club/",
     year: "2024",
-    color: "#3C91E6",
+    color: "rgb(60, 145, 230)",
+    resumecolor: "rgb(60, 145, 230,0.8)",
     codelink:
       "https://github.com/ScientificClubofESI/open-source-event-website",
   },
   {
     id: 1,
-
     type: "web",
     image: ccc,
     title: "ccc website",
+    resume:
+      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
     weblink: "",
     year: "2023",
-    color: "#392F5A",
+    color: "rgb(57, 47, 90)",
+    resumecolor:"rgb(57, 47, 90,0.8)",
     codelink: "https://github.com/ScientificClubofESI/ccc-website",
   },
   {
@@ -43,9 +48,12 @@ export const Works = [
     type: "web",
     image: chedmed,
     title: "chedmed app",
+    resume:
+      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
     weblink: "",
     year: "2023",
-    color: "#9DD9D2",
+    color: "rgb(157, 217, 210)",
+    resumecolor:"rgb(157, 217, 210,0.8)",
     codelink: "https://github.com/Amine2240/chedmed-front-end.git",
   },
   {
@@ -54,9 +62,12 @@ export const Works = [
     // reference: workref3,
     image: musicapp,
     title: "music app",
+    resume:
+      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
     weblink: "https://music-app-713c4c.netlify.app/",
     year: "2023",
-    color: "#89023E",
+    color: "rgb(137, 2, 62)",
+    resumecolor:"rgb(137, 2, 62,0.8)",
     codelink: "https://github.com/Amine2240/Music_App_with_react",
   },
   {
@@ -65,9 +76,12 @@ export const Works = [
     // reference: workref4,
     image: realestate,
     title: "real estate",
+    resume:
+      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
     weblink: "https://real-estate-88851b.netlify.app/",
     year: "2023",
-    color: "#884843",
+    color: "rgb(136, 72, 67)",
+    resumecolor:"rgb(136, 72, 67,0.8)",
     codelink: "https://github.com/Amine2240/real-estate-app",
   },
 
@@ -78,10 +92,13 @@ export const Works = [
     image: deliveryapp1,
     image2: deliveryapp2,
     title: "food delivery app",
+    resume:
+      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
     weblink:
       "https://github.com/Amine2240/food_delivery_mobile_app_with_flutter",
     year: "2023",
-    color: "#1F7F92",
+    color: "rgb(31, 127, 146)",
+    resumecolor:" rgb(31, 127, 146,0.8)",
     codelink:
       "https://github.com/Amine2240/food_delivery_mobile_app_with_flutter",
   },
@@ -91,9 +108,12 @@ export const Works = [
     // reference: workref2,
     image: movieapp,
     title: "movie app",
+    resume:
+      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
     weblink: "https://movies-clone-745341.netlify.app/",
     year: "2023",
     color: "#294659d3",
+    resumecolor: "#294659d3",
     codelink:
       "https://github.com/Amine2240/MERN-stack-Netflix-Clone-app-with-authentication",
   },
@@ -103,9 +123,12 @@ export const Works = [
     // reference: workref1,
     image: ecommerce,
     title: "ecommerce app",
+    resume:
+      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
     weblink: "https://ecommerce-react-app-amine.netlify.app/",
     year: "2023",
-    color: "#2B303A",
+    color: "rgb(43, 48, 58)",
+    resumecolor: "rgb(43, 48, 58,0.8)",
     codelink: "https://github.com/Amine2240/E-commerce-react-app",
   },
   {
@@ -114,9 +137,12 @@ export const Works = [
     // reference: workref0,
     image: businesscart,
     title: "business cart",
+    resume:
+      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
     weblink: "https://business-amine.netlify.app/",
     year: "2023",
-    color: "#EA9A27",
+    color: "rgb(234, 154, 39)",
+    resumecolor:"rgb(234, 154, 39,0.8)",
     codelink: "https://github.com/Amine2240/business-cart-react",
   },
 ];
--- client/src/components/workschema.jsx
+++ client/src/components/workschema.jsx
@@ -2,14 +2,30 @@
 /* eslint-disable react/jsx-no-target-blank */
 import { useDispatch } from "react-redux";
 import { sethoverbooleen } from "../redux/hoverslice";
+// import gsap from "gsap";
+// import ScrollTrigger from "gsap/ScrollTrigger";
+// gsap.registerPlugin(ScrollTrigger);
+// import { useEffect } from "react";
 
 const Workschema = ({ item }) => {
   const dispatch = useDispatch();
+  // useEffect(() => {
+  //   gsap.fromTo("item.reference",
+  //   {
+  //     y : -10,
+
+  //   },{
+  //     y : 0,
+  //     duration : 0.3,
+
+  //   })
+
+  // }, []);
   return (
     <div>
       <a href={item.weblink} target="_blank">
         <div
-          className={`sm:h-[550px] sm:w-[600px] h-[600px] w-[350px] cursor-pointer group  flex place-content-center items-center px-5 hover:rounded-[20px] transition-all `}
+          className={`sm:h-[550px] sm:w-[600px] h-[600px] w-[350px] cursor-pointer group  flex place-content-center items-center px-5 hover:rounded-[20px] transition-all relative overflow-hidden`}
           // data-cursor-icon='../assets/icons/facebook.png'
           onMouseOver={() => {
             dispatch(sethoverbooleen(true));
@@ -42,6 +58,16 @@ const Workschema = ({ item }) => {
           ) : (
             ""
           )}
+          <div
+            style={{
+              background: item.resumecolor,
+              backdropFilter: "blur(5px)",
+            }}
+            className={` w-full absolute shadow-2xl h-[300px] bottom-0 rounded-xl transition-all  p-5 flex flex-col place-content-around translate-y-[300px] group-hover:translate-y-0 duration-500  `}
+          >
+            <p className=" text-4xl capitalize text-white">{item.title} </p>
+            <p className=" text-white text-lg">{item.resume}</p>
+          </div>
         </div>
       </a>
       <p className=" text-4xl capitalize mt-5 font-semibold">{item.title}</p>
