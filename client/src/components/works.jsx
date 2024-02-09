1568cd01d7c5e6cb82bc2025aa088b283c6d19cf
kadoum amine <kadoumamine@gmail.com>
2024-02-09 23:15:47 +0100

    update some resumes

--- client/src/components/works.jsx
+++ client/src/components/works.jsx
@@ -22,7 +22,7 @@ export const Works = [
     image: opensource,
     title: "Open Source website",
     resume:
-      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
+      "this website is built with passion by collaboration of CSE developement departement members in a very small period of time, it is a platform that describes the open source day which is an event organized bu CSE, it is basically a day where participants learn and contributos to open source projects  ",
     weblink: "https://open-source-day-2023.cse.club/",
     year: "2024",
     color: "rgb(60, 145, 230)",
@@ -36,11 +36,11 @@ export const Works = [
     image: ccc,
     title: "ccc website",
     resume:
-      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
+      "this website is built using next js and tailwind css technologies , it stands for cse coding context which is a competition organized by cse members where problems are supposed to be solved by participants using the most recent techonologies and framworks",
     weblink: "",
     year: "2023",
     color: "rgb(57, 47, 90)",
-    resumecolor:"rgb(57, 47, 90,0.8)",
+    resumecolor: "rgb(57, 47, 90,0.8)",
     codelink: "https://github.com/ScientificClubofESI/ccc-website",
   },
   {
@@ -49,11 +49,11 @@ export const Works = [
     image: chedmed,
     title: "chedmed app",
     resume:
-      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
+      "chedmed is a web application built in collobariton by our team in a DEVCAMP (hackathon organized by CAPenp) , it suggests solutions about products returns in E-commerce fields, it is powered by an AI model , a simple chat that advices the client by giving him the appropriate datas",
     weblink: "",
     year: "2023",
     color: "rgb(157, 217, 210)",
-    resumecolor:"rgb(157, 217, 210,0.8)",
+    resumecolor: "rgb(157, 217, 210,0.8)",
     codelink: "https://github.com/Amine2240/chedmed-front-end.git",
   },
   {
@@ -62,12 +62,11 @@ export const Works = [
     // reference: workref3,
     image: musicapp,
     title: "music app",
-    resume:
-      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
+    resume: "",
     weblink: "https://music-app-713c4c.netlify.app/",
     year: "2023",
     color: "rgb(137, 2, 62)",
-    resumecolor:"rgb(137, 2, 62,0.8)",
+    resumecolor: "rgb(137, 2, 62,0.8)",
     codelink: "https://github.com/Amine2240/Music_App_with_react",
   },
   {
@@ -76,12 +75,11 @@ export const Works = [
     // reference: workref4,
     image: realestate,
     title: "real estate",
-    resume:
-      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
+    resume: "a full-stack web application ",
     weblink: "https://real-estate-88851b.netlify.app/",
     year: "2023",
     color: "rgb(136, 72, 67)",
-    resumecolor:"rgb(136, 72, 67,0.8)",
+    resumecolor: "rgb(136, 72, 67,0.8)",
     codelink: "https://github.com/Amine2240/real-estate-app",
   },
 
@@ -93,12 +91,12 @@ export const Works = [
     image2: deliveryapp2,
     title: "food delivery app",
     resume:
-      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
+      " a mobile application build with flutter framwork and integrated with food api,it provides informations about food including its price, its image, its original country, its way of cook and more...",
     weblink:
       "https://github.com/Amine2240/food_delivery_mobile_app_with_flutter",
     year: "2023",
     color: "rgb(31, 127, 146)",
-    resumecolor:" rgb(31, 127, 146,0.8)",
+    resumecolor: " rgb(31, 127, 146,0.8)",
     codelink:
       "https://github.com/Amine2240/food_delivery_mobile_app_with_flutter",
   },
@@ -109,7 +107,7 @@ export const Works = [
     image: movieapp,
     title: "movie app",
     resume:
-      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
+      "a full-stack movie application inspired by netflix,using the tmdb api that provides the most important informations about the movies, tv series and more.. , it also includes an authentication method (user password) and database storage (mongodb) ",
     weblink: "https://movies-clone-745341.netlify.app/",
     year: "2023",
     color: "#294659d3",
@@ -124,7 +122,7 @@ export const Works = [
     image: ecommerce,
     title: "ecommerce app",
     resume:
-      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
+      "a learn-purpose project made with react framwork, showing multiple types of kitmans with the ability of having a list of choices and being able to save them ",
     weblink: "https://ecommerce-react-app-amine.netlify.app/",
     year: "2023",
     color: "rgb(43, 48, 58)",
@@ -137,12 +135,11 @@ export const Works = [
     // reference: workref0,
     image: businesscart,
     title: "business cart",
-    resume:
-      "In summary, game development involves a multidisciplinary approach that combines creativity, technical expertise, collaboration, and strategic planning to create immersive and engaging interactive experiences. While designing and writing code are integral parts of game development, they are just .",
+    resume: "",
     weblink: "https://business-amine.netlify.app/",
     year: "2023",
     color: "rgb(234, 154, 39)",
-    resumecolor:"rgb(234, 154, 39,0.8)",
+    resumecolor: "rgb(234, 154, 39,0.8)",
     codelink: "https://github.com/Amine2240/business-cart-react",
   },
 ];
